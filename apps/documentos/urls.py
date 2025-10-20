a5ab0a543fea646aa854f0dd8ceb38f97b94ea5b
HenrryDG <henrrydga20@gmail.com>
2025-10-19 22:37:08 -0400

    Modificando el GET de documentos para obtener unicamente los que pertenecen a una solicitud

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('documentos/', views.documento_collection, name='documento-collection'),  # GET (listar)
+    path('documentos/<int:solicitud_id>/', views.documento_collection, name='documento-collection'),
     path('documentos/verificar-carnet/', views.verificar_carnet, name='verificar-carnet'),  # POST (verificar carnet)
     path('documentos/verificar-direccion/', views.verificar_direccion, name='verificar-direccion'),  # POST (verificar dirección)
 ]
