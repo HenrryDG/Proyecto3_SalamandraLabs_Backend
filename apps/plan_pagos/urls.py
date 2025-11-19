2213daccab9217c4758dee50673ccae8ff29724e
edsoncf7 <siblublu@gmail.com>
2025-11-19 17:58:13 -0400

    url para el endpoint de notificaciones

--- apps/plan_pagos/urls.py
+++ apps/plan_pagos/urls.py
@@ -4,4 +4,5 @@ from .views import listar_plan_pagos_por_prestamo, actualizar_plan_pago
 urlpatterns = [
     path('prestamos/<int:prestamo_id>/plan-pagos/', listar_plan_pagos_por_prestamo, name='listar_plan_pagos_por_prestamo'),
     path('plan-pagos/<int:plan_id>/', actualizar_plan_pago, name='actualizar_plan_pago'),
+    path('plan_pagos/notificaciones/', plan_pagos_notificaciones, name='notificaciones_plan_pagos'),
 ]
