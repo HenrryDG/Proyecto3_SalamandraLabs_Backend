fd90ac041eb012a03765521b799b69212eaca222
edsoncf7 <siblublu@gmail.com>
2025-11-19 18:00:34 -0400

    agregando libreria datetime para las notificaciones

--- apps/plan_pagos/views.py
+++ apps/plan_pagos/views.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from drf_spectacular.utils import extend_schema
+from datetime import date, timedelta
 
 from .models import PlanPago
 from .serializers import PlanPagoSerializer
